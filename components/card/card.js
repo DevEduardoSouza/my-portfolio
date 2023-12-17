f27fba436aa36e0a56c00787450f8954f604da89
Eduardo Souza <103686965+DevEduardoSouza@users.noreply.github.com>
2023-12-16 23:00:19 -0300

    feat: finaliza component card

--- components/card/card.js
+++ components/card/card.js
@@ -3,17 +3,17 @@ import { html } from "../../util/util.js";
 export function card(props) {
   if (!props) return "Error: Sem arqumentos";
   // Card Container
-  const cardContainer = html.createElementWithClasses("div", ".card-container");
+  const cardContainer = html.createElementWithClasses("div", "card-container");
 
   // Card Category
-  const cardCategory = html.createElementWithClasses("div", ".card-category");
+  const cardCategory = html.createElementWithClasses("div", "card-category");
   cardCategory.textContent = props.category;
 
   // Card Imgs
-  const cardImgs = html.createElementWithClasses("div", ".card-imgs");
-  const carousel = html.createElementWithClasses("div", ".carousel");
+  const cardImgs = html.createElementWithClasses("div", "card-imgs");
+  const carousel = html.createElementWithClasses("div", "carousel");
 
-  const imgCarousel = html.createElementWithClasses("img", ".img-carousel");
+  const imgCarousel = html.createElementWithClasses("img", "img-carousel");
   imgCarousel.src = props.imgs[0];
   imgCarousel.alt = props.title;
 
@@ -21,25 +21,25 @@ export function card(props) {
   cardImgs.appendChild(carousel);
 
   // Card Details
-  const cardDetails = html.createElementWithClasses("div", ".card-details");
+  const cardDetails = html.createElementWithClasses("div", "card-details");
 
-  const cardTitle = html.createElementWithClasses("div", ".card-title");
+  const cardTitle = html.createElementWithClasses("div", "card-title");
   cardTitle.innerHTML = `<h3>${props.title}</h3>`;
 
   const cardDescription = html.createElementWithClasses(
     "div",
-    ".card-description"
+    "card-description"
   );
   cardDescription.innerHTML = `<p>${props.description}</p>`;
 
   const stackViewMore = html.createElementWithClasses(
     "div",
-    ".stack-view-more"
+    "stack-view-more"
   );
 
   const stackContainer = html.createElementWithClasses(
     "div",
-    ".stack-container"
+    "stack-container"
   );
   props.stacks.forEach((element) => {
     stackContainer.innerHTML += `<i title="${element}" class="devicon-${element}-plain colored"></i>`;
@@ -48,8 +48,8 @@ export function card(props) {
 
   const btnViewMore = html.createElementWithClasses(
     "button",
-    ".btn",
-    ".btn-view-more"
+    "btn",
+    "btn-view-more"
   );
   btnViewMore.textContent = "Ver mais >";
   stackViewMore.appendChild(btnViewMore);
