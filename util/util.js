9ab793393bede6baf92f947b06e914339f95ca45
Eduardo Souza <103686965+DevEduardoSouza@users.noreply.github.com>
2023-12-16 21:52:32 -0300

    feat: Adiciona utilitário para manipulação de elementos HTML

---
+++ util/util.js
@@ -0,0 +1,26 @@
+/**
+ * Um objeto utilitário para manipulação de elementos HTML.
+ * @namespace
+ * @description Este objeto fornece métodos para interagir com elementos HTML de forma simplificada.
+ */
+export const html = {
+  get(selector) {
+    return document.querySelector(selector);
+  },
+  getAll(selector) {
+    return document.querySelectorAll(selector);
+  },
+  create(element) {
+    return document.createElement(element);
+  },
+  createElementWithClasses(element, ...classes) {
+    if (!element) return null;
+
+    const elementWithClasses = document.createElement(element);
+    if (classes.length > 0) {
+      elementWithClasses.classList.add(...classes);
+    }
+
+    return elementWithClasses;
+  },
+};
