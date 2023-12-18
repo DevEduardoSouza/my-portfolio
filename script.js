522712bfdaa1b2e69bf81d436a69bdce32022b1f
Eduardo Souza <103686965+DevEduardoSouza@users.noreply.github.com>
2023-12-18 09:41:49 -0300

    feat: add função para preencher perfil da pessoa

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
 import { card } from "./components/card/card.js";
 import { html } from "./util/util.js";
 
-const btnMobile =document.getElementById('btn-mobile');
-function toggleMenu(event){
-    if (event.type == 'touchstart') event.preventDefault();
-        const nav2 = document.getElementById('conteiner');
-        nav2.classList.toggle('active'); 
-        const active = nav2.classList.contains('active');
-        event.currentTarget.setAttribute('arial-expended', active);
-        if(active){
-            event.currentTarget.setAttribute('aria-label' , 'Fechar menu')
-        } else{
-            event.currentTarget.setAttribute('aria-label' , 'Abrir menu')
-        }
+const btnTogglePersonDetails = html.get(".btn-toggle");
+const personDetails = html.get(".person-details");
+
+btnTogglePersonDetails.addEventListener("click", () => {
+  html.handleToggle(personDetails);
+});
+
+// fillProfile
+const data = {
+  name: "Eduardo de Souza",
+  profession: "desenvolvedor full stack",
+  residence: "Brasil",
+  state: "Bahia",
+  age: "22",
+  languages: [
+    "javascript",
+    "react",
+    "typescript",
+    "html5",
+    "css3",
+    "nodejs",
+    "java",
+    "c",
+    "spring",
+    "mongodb",
+    "mysql",
+    "postgresql",
+    "express",
+  ],
+  secondaryStack: ["Bootstrap", "GIT e GITHUB", "Responsividade"],
+};
+
+function fillProfile(data) {
+  const name = (html.get(".name").innerHTML = data.name);
+  const profession = (html.get(".profession").innerHTML = data.profession);
+  const residence = (html.get(".residence .value").innerHTML = data.residence);
+  const state = (html.get(".state .value").innerHTML = data.state);
+  const age = (html.get(".age .value").innerHTML = data.age);
+
+  
+  // languages
+  const languagesList = html.get(".list-languages");
+  data.languages.forEach((language) => {
+    languagesList.innerHTML += `<i title="${language}" class="devicon-${language}-plain colored"></i>`;
+  });
+
+
+  // secondaryStack
+  const stacksList = html.get(".list-stacks ul");
+  data.secondaryStack.forEach((stack) => {
+    stacksList.innerHTML += `
+    <li class="stack">
+        <i class="bi bi-patch-check-fill"></i>
+        <span>${stack}</span>
+    </li>`;
+  });
 }
-
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
-
-
-
-
-
-
+fillProfile(data);
