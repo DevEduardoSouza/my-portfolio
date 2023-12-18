1dd5c052ccfa38c564bce4c54a257833e449a004
Eduardo Souza <103686965+DevEduardoSouza@users.noreply.github.com>
2023-12-18 18:34:49 -0300

    feat: add dados de exemplos de projetos

---
+++ data/data.js
@@ -0,0 +1,94 @@
+export const data = {
+  name: "Eduardo de Souza",
+  profession: "desenvolvedor full stack",
+  residence: "Brasil",
+  state: "Bahia",
+  age: "22",
+  languages: [
+    "javascript",
+    "react",
+    "typescript",
+    "html5",
+    "css3",
+    "nodejs",
+    "java",
+    "c",
+    "spring",
+    "mongodb",
+    "mysql",
+    "postgresql",
+    "express",
+  ],
+  secondaryStack: ["Bootstrap", "GIT e GITHUB", "Responsividade"],
+  projects: [
+    {
+      title: "Portfolio Website",
+      description: "My personal portfolio showcasing my skills and projects.",
+      languages: ["html5", "css3", "javascript"],
+      category: "front-end",
+      imgs: [
+        "https://www.pixground.com/wp-content/uploads/2023/05/Windows-11-Abstract-Bloom-4K-Wallpaper-1024x576.png.webp",
+        "portfolio-img2.jpg",
+        "portfolio-img3.jpg",
+      ],
+      links: [
+        {
+          deploy: "https://www.example.com",
+          github: "https://github.com/yourusername/portfolio",
+        },
+      ],
+    },
+    {
+      title: "Task Manager App",
+      description:
+        "A simple task manager web application with CRUD functionality.",
+      languages: ["javascript", "react", "nodejs", "mongodb"],
+      category: "back-end",
+      imgs: [
+        "https://www.pixground.com/wp-content/uploads/2023/05/Windows-11-Abstract-Bloom-4K-Wallpaper-1024x576.png.webp",
+        "task-manager-img2.jpg",
+        "task-manager-img3.jpg",
+      ],
+      links: [
+        {
+          deploy: "https://task-manager-app.herokuapp.com",
+          github: "https://github.com/yourusername/task-manager-app",
+        },
+      ],
+    },
+    {
+      title: "E-commerce Website",
+      description: "A fully responsive e-commerce website with product listings, shopping cart, and checkout functionality.",
+      languages: ["html5", "css3", "javascript", "react", "nodejs", "mongodb"],
+      category: "full-stack",
+      imgs: [
+        "https://www.pixground.com/wp-content/uploads/2023/05/Windows-11-Abstract-Bloom-4K-Wallpaper-1024x576.png.webp",
+        "ecommerce-img2.jpg",
+        "ecommerce-img3.jpg",
+      ],
+      links: [
+        {
+          deploy: "https://ecommerce-website.herokuapp.com",
+          github: "https://github.com/yourusername/ecommerce-website",
+        },
+      ],
+    },
+    {
+      title: "Weather App",
+      description: "A weather application that fetches real-time weather data based on user input and displays it in a user-friendly interface.",
+      languages: ["html5", "css3", "javascript", "react"],
+      category: "front-end",
+      imgs: [
+        "https://www.pixground.com/wp-content/uploads/2023/05/Windows-11-Abstract-Bloom-4K-Wallpaper-1024x576.png.webp",
+        "weather-app-img2.jpg",
+        "weather-app-img3.jpg",
+      ],
+      links: [
+        {
+          deploy: "https://weather-app-example.com",
+          github: "https://github.com/yourusername/weather-app",
+        },
+      ],
+    },
+  ],
+};
