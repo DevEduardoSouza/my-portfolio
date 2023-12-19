0f60c81883278c1d5784f7e6474032cef1306265
Eduardo Souza <103686965+DevEduardoSouza@users.noreply.github.com>
2023-12-19 18:07:41 -0300

    feat: add função para criar uma 'message' personalizada

--- util/util.js
+++ util/util.js
@@ -26,4 +26,24 @@ export const html = {
   handleToggle: (element) => {
     element.classList.toggle("hide");
   },
+  message: ({ text, icon }) => {
+    if (!text || !icon) {
+      return;
+    }
+    const message = html.createElementWithClasses("div", "message");
+    message.style.textAlign = "center";
+    message.style.margin = "auto";
+
+    const iconMsg = html.createElementWithClasses("div", "icon");
+    iconMsg.style.fontSize = "2rem";
+    iconMsg.innerHTML = icon;
+
+    const textMsg = html.createElementWithClasses("div", "text");
+    iconMsg.style.fontSize = "2rem";
+    textMsg.innerHTML = text;
+
+    [iconMsg, textMsg].forEach((e) => message.appendChild(e));
+
+    return message;
+  },
 };
